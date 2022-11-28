853cfc888d4f807445dd1ddb9c1feb455a05fe90
priyakesarwani0101 <priyakesarwani0101@gmail.com>
2022-11-28 16:50:19 +0530

    menu_page created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Menu from "./components/menu_page/menu";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Menu />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +19,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
     </div>
   );
 }
