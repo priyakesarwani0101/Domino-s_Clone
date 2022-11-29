97e15675e6d10c7ff40c93797fbcbb38fb39506b
priyakesarwani0101 <priyakesarwani0101@gmail.com>
2022-11-29 12:21:30 +0530

    menu page updated

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Menu from "./components/menu_page/menu";
 import Navbar from "./components/navbar/Navbar/Navbar.jsx";
+import NavbarContext from './components/navbar/Context/NavbarContext'
 
 function App() {
   return (
     <div className="App">
      {/* <Navbar /> */}
+     <NavbarContext />
       <Menu />
      
 
