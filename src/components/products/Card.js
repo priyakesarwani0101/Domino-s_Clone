0cf72a50344847c2bcb2dde9d712ea952b6a0566
priyakesarwani0101 <priyakesarwani0101@gmail.com>
2022-11-30 18:44:35 +0530

    product page updated

--- src/components/products/Card.js
+++ src/components/products/Card.js
@@ -15,7 +15,7 @@ const Cards = ({props}) => {
      <b> <h3>{props.title}</h3></b>
       
       <h3>Rs: {props.price}</h3>
-       <h3>Ratings: {props.ratings } </h3>
+       <h3>Ratings: {props.ratings } <i class="fa-solid fa-star"></i></h3>
       <div id="cartBtn">
         <button onClick={addedToCart}>add to cart</button>
       </div>
