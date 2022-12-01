8a5fe567a8712bff1062b3fabedaaa88f9ac1f98
priyakesarwani0101 <priyakesarwani0101@gmail.com>
2022-12-01 11:24:27 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@
 // import  Cards  from './components/products/Card';
 // import React from "react";
 import './App.css';
-import NavbarContext from './components/navbar/Context/NavbarContext'
-// import Product from './components/products/product.js'
+
+
 
 function App() {
   return (
     <div className="App">
-      
-      
      
      
       
