2527319df6611a9ee18f6f0abf38075f443cc77f
sanjay121531 <71871677+sanjayloncha@users.noreply.github.com>
2022-12-01 13:55:25 +0530

    home-page updated

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import './App.css';
 import Detailsp from "./components/product_details/Detailsp"
 import { ChakraProvider } from '@chakra-ui/react'
-import Home from "./components/landing_pg/Home"
 
 import 'bootstrap/dist/css/bootstrap.min.css';
  
@@ -10,7 +9,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App" >
-      <Home />
      </div>
   );
 }
