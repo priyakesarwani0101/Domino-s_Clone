7f833816bc49a3bc5d01fffc2aff925dd9058bbb
sanjay121531 <71871677+sanjayloncha@users.noreply.github.com>
2022-12-01 16:47:47 +0530

    login started

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import Detailsp from "./components/product_details/Detailsp"
 import { ChakraProvider } from '@chakra-ui/react'
+import Login from './components/landing_pg/Login/Login';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
  
@@ -9,6 +10,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App" >
+      <Login />
      </div>
   );
 }
---
+++ src/components/landing_pg/Login/Login.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+import "./Login.css"
+
+export default function login() {
+
+    const check = ()=>{
+        
+    }
+    
+
+  return (
+    <div>
+        <div>
+            <img src='https://pizzaonline.dominos.co.in/static/assets/Dominos_logo.svg' alt='logo' />
+            <img src='https://pizzaonline.dominos.co.in/static/assets/login_pizza_image.png' alt='pizza' />
+        </div>
+        <div className='hd-img' >
+            <div className='hd-img_txt' >
+                <span>Login</span>
+                <p>to unlock awesome new feature</p>
+            </div>
+            <div className='hd-ing_sbtxt' >
+                <div>
+                    <img src='https://pizzaonline.dominos.co.in/static/assets/icons/great_food.svg' alt='great-food' />
+                    <div>
+                        <p>Great</p>
+                        <p>Food</p>
+                    </div>
+                </div>
+                <div>
+                <img src='https://pizzaonline.dominos.co.in/static/assets/icons/great_offers.svg' alt='great-offer' />
+                    <div>
+                        <p>Great</p>
+                        <p>Offers</p>
+                    </div>
+                </div>
+                <div>
+                <img src='https://pizzaonline.dominos.co.in/static/assets/icons/easy_reorder.svg' alt='easy-food' />
+                    <div>
+                        <p>Easy</p>
+                        <p>Reordering</p>
+                    </div>
+                </div>
+            </div>
+
+            <div id='login-div'>
+                <div>Login with your valid mobile number</div>
+                <div className='inputMobile'>
+                    <div>
+                        <input className='code' type="type" placeholder="+91" />
+                    </div>
+                    <div>
+                        <input className='mob' type="text" maxLength="10" placeholder='Enter Mobile number' />
+                    </div>
+                </div>
+                <div>
+                    <button onClick={check} id='submit-btn'>submit</button>
+                </div>
+            </div>
+
+        </div>
+    </div>
+  )
+}
