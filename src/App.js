a035391c4477229ee2333f25d9af8069a75819d7
sanjay121531 <71871677+sanjayloncha@users.noreply.github.com>
2022-12-02 20:34:03 +0530

    login completed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Detailsp from "./components/product_details/Detailsp"
 import { ChakraProvider } from '@chakra-ui/react'
 import Login from './components/landing_pg/Login/Login';
+import Home from './components/landing_pg/Home';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
  
@@ -10,7 +11,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App" >
+      {/* <ChakraProvider>
       <Login />
+      </ChakraProvider> */}
+      <Home />
      </div>
   );
 }
--- src/components/landing_pg/Login/Login.js
+++ src/components/landing_pg/Login/Login.js
@@ -1,73 +1,143 @@
-import { Alert } from 'bootstrap';
-import React, { useState } from 'react'
-import "./Login.css"
+import {
+    Button,
+    Flex,
+    FormControl,
+    FormLabel,
+    Heading,
+    Input,
+    Stack,
+    useColorModeValue,
+    PinInput, PinInputField, HStack
+  } from '@chakra-ui/react';
+  import "./Login.css"
+  
+  import React ,{ useState } from 'react';
 
-export default function login() {
-    
-    // let [otp,setOtp] = useState() ;
+  
+  export default function ResetPasswordForm() {
 
-    const generateOTP = ()=>{
+    let [otp,setOtp] = useState('') ;
+    let [mob,setMob] = useState("") ;
+    let [flag,setFlag] = useState(true) ;
+
+    let [eOTP,setEOTP] = useState('') ;
+
+
+
+
+    const check = (e)=>{
+        
+        setMob(e.target.value) ;
+    }
+
+    const getOTP = ()=>{
+
+        if(mob.length < 10){
+            setMob("") ;
+            alert("enter valid number") ;
+            return ;
+        }else{
+            for(let i=0;i<10;i++){
+                if(mob[i] >= '0' && mob[i] <='9'){
+                    continue
+                }else{
+                    setMob("") ;
+                    alert("enter valid number!") ;
+                    return ;
+                }
+            }
+        }
+        
         let digit = '0123456789' ;
         let OTP = '' ;
+
         for(let i=0;i<4;i++){
             OTP += digit[Math.floor(Math.random()*10)] ;
         }
+
+        setOtp(OTP) ;
+        setFlag(false) ;
+
         alert(OTP) ;
+
     }
+
+    const compare = ()=>{
+        console.log(otp) ;
+        console.log(eOTP) ;
+
+
+        if(otp == eOTP){
+            alert("login Success!") ;
+
+        }else{
+            alert("wrong OTP entered");
+            setEOTP('') ;
+            setFlag(true) ;
+        }
+    }
+
+    return (
+      <Flex
+        minH={'100vh'}
+        align={'center'}
+        justify={'center'}
+        bg={useColorModeValue('gray.50', 'gray.800')}>
+        <Stack
+          spacing={4}
+          w={'full'}
+          maxW={'md'}
+          bg={useColorModeValue('white', 'gray.700')}
+          rounded={'xl'}
+          boxShadow={'lg'}
+          p={6}
+          my={12}>
+          <Heading lineHeight={1.1} fontSize={{ base: '2xl', md: '3xl' }}>
+            Enter valid number
+          </Heading>
+          <FormControl id="email" isRequired>
+            <FormLabel>Enter Mobile number</FormLabel>
+            <Input
+            onChange={check}
+            value={mob}
+            maxLength={10}
+              placeholder="Mob No."
+              _placeholder={{ color: 'gray.500' }}
+              type="email"
+            />
+          </FormControl>
+
+          <div>
+          {
+            flag ?   <Stack spacing={6}>
+            <Button
+            onClick={getOTP}
+              bg={'blue.400'}
+              color={'white'}
+              _hover={{
+                bg: 'blue.500',
+              }}>
+              Submit
+            </Button>
+          </Stack>  :  <div> 
+
+            {/* <input type={"text"} onChange={eOTP} maxLength='4' value={enterOTP} placeholder='enter otp' /> <button onClick={compare} >submit</button>  */}
+            <HStack>
+                <PinInput  placeholder='_'  >
+                    <PinInputField onChange={(e) => setEOTP( eOTP + e.target.value) } />
+                    <PinInputField onChange={(e) => setEOTP( eOTP + e.target.value) } />
+                    <PinInputField onChange={(e) => setEOTP( eOTP + e.target.value) } />
+                    <PinInputField onChange={(e) => setEOTP( eOTP + e.target.value) } />
+                    <button id="verifyBtn"  onClick={compare} >Verify</button>
+                </PinInput>
+            </HStack> 
+
+            </div>
     
+          }
+          </div>
 
-  return (
-    <div>
-        <div>
-            <img src='https://pizzaonline.dominos.co.in/static/assets/Dominos_logo.svg' alt='logo' />
-            <img src='https://pizzaonline.dominos.co.in/static/assets/login_pizza_image.png' alt='pizza' />
-        </div>
-        <div className='hd-img' >
-            <div className='hd-img_txt' >
-                <span>Login</span>
-                <p>to unlock awesome new feature</p>
-            </div>
-            <div className='hd-ing_sbtxt' >
-                <div>
-                    <img src='https://pizzaonline.dominos.co.in/static/assets/icons/great_food.svg' alt='great-food' />
-                    <div>
-                        <p>Great</p>
-                        <p>Food</p>
-                    </div>
-                </div>
-                <div>
-                <img src='https://pizzaonline.dominos.co.in/static/assets/icons/great_offers.svg' alt='great-offer' />
-                    <div>
-                        <p>Great</p>
-                        <p>Offers</p>
-                    </div>
-                </div>
-                <div>
-                <img src='https://pizzaonline.dominos.co.in/static/assets/icons/easy_reorder.svg' alt='easy-food' />
-                    <div>
-                        <p>Easy</p>
-                        <p>Reordering</p>
-                    </div>
-                </div>
-            </div>
-
-            <div id='login-div'>
-                <div>Login with your valid mobile number</div>
-                <div className='inputMobile'>
-                    <div>
-                        <input className='code' type="type" placeholder="+91" />
-                    </div>
-                    <div>
-                        <input className='mob' type="text" maxLength="10" placeholder='Enter Mobile number' />
-                    </div>
-                </div>
-                <div>
-                    <button onClick={generateOTP} id='submit-btn'>submit</button>
-                </div>
-
-            </div>
-
-        </div>
-    </div>
-  )
-}
+        </Stack>
+      </Flex>
+    );
+  }
