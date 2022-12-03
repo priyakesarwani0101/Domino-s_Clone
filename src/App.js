b9cf4c3fccd042df29d06220b7815023a5d87083
sanjay121531 <71871677+sanjayloncha@users.noreply.github.com>
2022-12-04 01:23:13 +0530

    home page updated

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@
 import './App.css';
 import Detailsp from "./components/product_details/Detailsp"
 import { ChakraProvider } from '@chakra-ui/react'
+import Home from "./components/landing_pg/Home"
+import Login from "./components/landing_pg/Login/Login"
+
 
 
 
@@ -11,7 +14,8 @@ function App() {
   return (
     <div className="App" >
       
-        
+        {/* <Home /> */}
+        <Login/>
        
      </div>
   );
