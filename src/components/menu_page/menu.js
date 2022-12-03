ebdacdf826ee14db40b6e3c19fbff156870b705a
priyakesarwani0101 <priyakesarwani0101@gmail.com>
2022-12-03 20:53:22 +0530

    menupage

--- src/components/menu_page/menu.js
+++ src/components/menu_page/menu.js
@@ -95,3 +95,53 @@ const Menu = () => {
 };
 
 export default Menu;
+
+
+
+
+// import  Cards  from './components/products/Card';
+// import React from "react";
+// import './App.css';
+// import NavbarContext from "./components/navbar/Context/NavbarContext";
+// import Footer from "./components/footer/Footer/Footer";
+// import {Routes,Route} from 'react-router-dom'
+// import Product from './components/products/product'
+// import VegPizza from "./components/products/veg_pizza";
+// import NonvegPizza from "./components/products/non_veg_pizza";
+// import PizzaMania from "./components/products/pizza_mania";
+// import Pasta from "./components/products/pasta";
+// import Beverages from "./components/products/beverages";
+// import Menu from './components/menu_page/menu'
+
+
+
+// function App() {
+
+
+//   return (
+//     <div className="App">
+      
+
+      {/* <NavbarContext />
+     
+
+      <Routes>
+        <Route path="/menu" element={<Product />}></Route>
+       <Route path="/menu/veg_pizza" element={<VegPizza />}></Route>
+       <Route path="/menu/non_veg_pizza" element={<NonvegPizza />}></Route>
+       <Route path="/menu/pizza_mania" element={<PizzaMania />}></Route>
+       <Route path="/menu/pasta" element={<Pasta />}></Route>
+       <Route path="/menu/sides_beverages" element={<Beverages />}></Route>
+         
+
+      </Routes>
+
+     <Footer /> */}
+
+//     </div>
+
+//   );
+// }
+
+// export default App;
+
