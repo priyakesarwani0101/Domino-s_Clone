aac7a9e6a2b80f65a466ed4ef8802b8de3121939
Ayushsingh05 <singhayush28321@gmail.com>
2022-12-05 01:44:09 +0530

    chechout functionality completed

--- src/components/cart/Address.js
+++ src/components/cart/Address.js
@@ -1,10 +1,29 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
  import './Address.css'
  import {Link} from 'react-router-dom'
 import { addressContext } from './addressContext/AddressContext'
+import { calcLength } from 'framer-motion';
 function Address() {
   const {openaddress} =useContext(addressContext);
   const {setopenAdress} =useContext(addressContext);
+  const dataObj={
+    firstName:"",
+    lastName:"",
+    email:"",
+    address:"",
+    city:""
+  }
+  const [data,setdata]=useState(dataObj);
+  const handleChange=(e)=>{
+    setdata({...data,[e.target.name]:e.target.value})
+  }
+  const handleClick =()=>{
+
+    console.log(data);
+    localStorage.setItem("userAddress",JSON.stringify(data));
+    setopenAdress(false);
+    setdata(dataObj);
+  }
   return (
     <div className='address_container-pk' style={openaddress ? {marginRight:"0px"} : {marginRight:"-502px"} }>
         <div>
@@ -13,22 +32,22 @@ function Address() {
                 <p>Fill deatils below</p>
                 <div className='al-inp-cont-pk'>
                     <div className='flex-inp-div-pk'>
-                        <input className='adrs-inp-tag-pk' type='text' placeholder='First name'></input>
-                        <input className='adrs-inp-tag-pk' type='text' placeholder='Last name'></input>
+                        <input className='adrs-inp-tag-pk' type='text' placeholder='First name' name='firstName' onChange={handleChange}></input>
+                        <input className='adrs-inp-tag-pk' type='text' placeholder='Last name' name="lastName"  onChange={handleChange}></input>
                     </div>
                     <div>
-                        <input className='adrs-inp-tag-pk width100-pk' type='email' placeholder='Email'></input>
+                        <input className='adrs-inp-tag-pk width100-pk' type='email' placeholder='Email' name='email'  onChange={handleChange}></input>
                     </div>
                     <div className='flex-inp-div-pk'>
-                        <input className='adrs-inp-tag-pk' type='text' placeholder='Flat No./Building name/Landmark'></input>
-                        <input className='adrs-inp-tag-pk' type='text' placeholder='City'></input>
+                        <input className='adrs-inp-tag-pk' type='text' placeholder='Flat No./Building name/Landmark' name='address' onChange={handleChange}></input>
+                        <input className='adrs-inp-tag-pk' type='text' placeholder='City' name='city' onChange={handleChange}></input>
                     </div>
                 </div>
             </div>
-            <div className='adrs-save-btn-div-pk'><Link to="/payment"><button onClick={()=>setopenAdress(false)}>SAVE & CONTINUE</button></Link></div>
+            <div className='adrs-save-btn-div-pk'><Link to="/payment"><button onClick={handleClick}>SAVE & CONTINUE</button></Link></div>
         </div>
     </div>
   )
 }
-
+// setopenAdress(false)
 export default Address
