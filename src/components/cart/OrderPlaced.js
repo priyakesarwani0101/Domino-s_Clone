3a443d672cac16be0fc234ea6b322a7dfaf3e644
pradipk1 <pradipshukla12699@gmail.com>
2022-12-04 15:58:28 +0530

    order placed UI created

--- src/components/cart/OrderPlaced.js
+++ src/components/cart/OrderPlaced.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import './PaymenDone.css'
 
-function PaymentDone() {
+import './PaymentDone.css'
+
+function OrderPlaced() {
   return (
     <div className='ord-succ-cont-pk'>
         <div>
-            <img src='https://nrifuture.com/wp-content/uploads/2022/03/image_processing20190812-2176-vehbsi.gif' />
+            <img src='https://i.pinimg.com/originals/32/b6/f2/32b6f2aeeb2d21c5a29382721cdc67f7.gif' />
             <h1>THANK YOU!</h1>
-            <h3>Payment done Successfully!</h3>
+            <h3>Order placed Successfully!</h3>
             <span>You will be redirected to the home page shortly or click here to return to home page</span>
             <button><span>Home</span></button>
         </div>
@@ -15,4 +16,4 @@ function PaymentDone() {
   )
 }
 
-export default PaymentDone
+export default OrderPlaced
