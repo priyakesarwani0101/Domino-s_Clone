86e624bc0abe36c5947127c8189c869881fc8896
Samrat Kumar <sam9999950@gmail.com>
2022-12-05 19:01:29 +0530

    loading

--- src/components/product_details/Loading.jsx
+++ src/components/product_details/Loading.jsx
@@ -1,38 +1,20 @@
 import React from 'react'
-import {Box,Image,Flex} from '@chakra-ui/react'
+import {Box,Image,Flex, Heading} from '@chakra-ui/react'
+import priya from './Gif/priya.gif.gif'
 
 const Loading = () => {
-    const outerBoxStyles = {
-        boxSize: '100%',
-        height:'730px',
-        p: '10',
-        background:
-          'url(https://media.tenor.com/tNFIwasgcS4AAAAC/pizza-delivery-spongebob-squarepants.gif) center/cover no-repeat',
-      }
-    
-      const innerBoxStyles = {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        textAlign: 'center',
-        boxSize: 'full',
-        color: 'white',
-        textShadow: '0 0 20px black',
-        fontWeight: 'bold',
-        fontSize: '20px',
-      }
+     
       return (
-        <Flex flexWrap='wrap' gap='24px' justifyContent='space-evenly'>
-          {/* adding backdrop-filter property to the element */}
+         
           
-          {/* adding backdrop-blur property to the element */}
-          <Box sx={outerBoxStyles}>
-            <Box sx={innerBoxStyles} backdropFilter='auto' backdropBlur='8px'>
-              <Image src='https://www.icegif.com/wp-content/uploads/pizza-icegif.gif' alt='gif'/>
+            <Box  h='1200px'>
+              <Image w='100%' h='100%' m='auto'   src={priya} alt='gif'/>
+               {/* <Heading  fontSize='100px' letterSpacing='2px' color='blue' textAlign='center'>Thank You From Domino'S Team</Heading> */}
             </Box>
-          </Box>
+            
            
-        </Flex>
+           
+         
       )
     }
 
